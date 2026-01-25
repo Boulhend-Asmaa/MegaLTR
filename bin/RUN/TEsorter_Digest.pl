108c785a19ad4ad1b7edca0d383eba8c2d717624
Asmaa Boulhend <boulhend.asmaa0gmail.com>
2026-01-25 18:03:50 +0100

    Fix TEsorter_Digest.pl ID format mismatch
    TEsorter adds suffixes like "#LTR/Copia" or "_INT#LTR/Copia" to
    sequence IDs, causing mismatch with LTRDIGEST tabout IDs.
    
    Changes:
    - Extract base ID by removing TEsorter classification suffix (#...)
    - Remove _INT suffix if present
    - Handle LTR_RETRIEVER format (seqid:start..end) conversion
    - Use hash for O(1) lookup instead of O(n) array search

--- bin/RUN/TEsorter_Digest.pl
+++ bin/RUN/TEsorter_Digest.pl
@@ -1,30 +1,47 @@
 use strict;
-my $searchfile = $ARGV[0];
-my $searchfor  = $ARGV[1];
-my @parray;
+my $searchfile = $ARGV[0];  # tabout.tsv (from LTRDIGEST)
+my $searchfor  = $ARGV[1];  # LTR.tsv (from TEsorter)
+
+# Build hash for O(1) lookup instead of O(n) array search
+my %parray;
 open( PFILE, "<$searchfile" );
 while (<PFILE>) {
     chomp();
     if (/(\S+)\t([\S|\s]+)/) {
         my $id   = $1;
-        my $rnu   = $2;
-        push( @parray, [ $id, $rnu] );
+        my $rnu  = $2;
+        $parray{$id} = $rnu;
     }
 }
 close PFILE;
+
 open( GFILE, "<$searchfor" );
 while (<GFILE>) {
     chomp();
     if (/(\S+)\t([\S|\s]+)/) {
         my $contig  = $1;
         my $gstart  = $2;
-        for my $i ( 0 .. $#parray ) 
-         {          
-       if($contig eq $parray[$i][0])    
-                {   
-         print "$parray[$i][0]\t$parray[$i][1]\t$gstart\n";
-       }
-         } 
+
+        # Extract base ID from TEsorter format
+        # TEsorter adds suffixes like "#LTR/Copia" or "_INT#LTR/Copia"
+        # Also handle LTR_RETRIEVER format: "seqid:start..end_INT#LTR/Family"
+        my $base_id = $contig;
+
+        # Remove TEsorter classification suffix (everything after #)
+        $base_id =~ s/#.*//;
+
+        # Remove _INT suffix if present
+        $base_id =~ s/_INT$//;
+
+        # Handle LTR_RETRIEVER format: convert "seqid:start..end" to "seqid_start_end"
+        if ($base_id =~ /^(\S+):(\d+)\.\.(\d+)$/) {
+            $base_id = "${1}_${2}_${3}";
+        }
+
+        # Look up in hash (O(1) instead of O(n))
+        if (exists $parray{$base_id}) {
+            print "$base_id\t$parray{$base_id}\t$gstart\n";
         }
     }
+}
 close GFILE;
